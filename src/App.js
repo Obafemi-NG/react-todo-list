a8a724d2e38091b470bae0778c1f103af9d33bfe
Obafemi <72113368+Obafemi-NG@users.noreply.github.com>
2021-05-31 18:07:56 +0100

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = 'container'>
+        <div className = 'row'>
+          <div className = 'col-4'> Yooo!!</div>
+          <div className = 'col-4'> Yo!</div>
+          <div className = 'col-4'> Yoo!! there!?</div>
+        </div>
+      </div>
     </div>
   );
 }
