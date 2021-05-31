2feef5720a179d64ef10b7118cd091467df07c72
Obafemi <72113368+Obafemi-NG@users.noreply.github.com>
2021-05-31 21:04:21 +0100

    updated components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
 
-function App() {
-  return (
-    <div className="App">
-      <div className = 'container'>
-        <div className = 'row'>
-          <div className = 'col-4'> Yooo!!</div>
-          <div className = 'col-4'> Yo!</div>
-          <div className = 'col-4'> Yoo!! there!?</div>
+export default class App extends Component {
+  state = {
+    items: [],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle submit");
+  };
+  handleClearList = () => {
+    console.log("handle clear list");
+  };
+  handleEdit = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+  handleRemove = (id) => {
+    console.log(`handle delete ${id}`);
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-4">
+            <h2 className="text-center text-capitalize">todo list</h2>
+          </div>
+          <TodoInput
+            item={this.state.item}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            editItem={this.state.editItem}
+          />
+          <TodoList
+            items={this.state.items}
+            handleEdit={this.handleEdit}
+            handleRemove={this.handleRemove}
+            handleClearList={this.handleClearList}
+          />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
